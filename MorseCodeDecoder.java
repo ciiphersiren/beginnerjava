0a2ff29b3c616408b793530bc42a70c03aa69641
Reet Mishra <145483913+ciiphersiren@users.noreply.github.com>
2024-06-11 13:01:15 +0530

    Add files via upload

--- MorseCodeDecoder.java
+++ MorseCodeDecoder.java
@@ -1,26 +1,28 @@
+import java.util.Scanner;
+
 public class MorseCodeDecoder {
 
     public static String decode(String morseCode) {
-        String[] english = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
-                             "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", 
-                             "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", 
-                             "5", "6", "7", "8", "9", "0", ",", ".", "?","!", "SOS"};
+        String[] english = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+                "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+                "u", "v", "w", "x", "y", "z", "1", "2", "3", "4",
+                "5", "6", "7", "8", "9", "0", ",", ".", "?", "!", "SOS" };
 
-        String[] morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", 
-                         ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", 
-                         "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", 
-                         "..---", "...--", "....-", ".....", "-....", "--...", "---..", 
-                         "----.", "-----", "--..--", ".-.-.-", "..--..", "-.-.--", "...---..."};
-        String[] words = morseCode.split("   ");
+        String[] morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+                ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
+                "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----",
+                "..---", "...--", "....-", ".....", "-....", "--...", "---..",
+                "----.", "-----", "--..--", ".-.-.-", "..--..", "-.-.--", "...---..." };
+        String[] words = morseCode.split(" / ");
         String trString = "";
-        for(String l: words){
+        for (String l : words) {
             String[] letters = l.split(" ");
-            for(String m: letters){
-                for(int i = 0; i<morse.length; i++)
-                if(m.equals(morse[i])){
-                    trString += english[i];
-                    break;
-                }
+            for (String m : letters) {
+                for (int i = 0; i < morse.length; i++)
+                    if (m.equals(morse[i])) {
+                        trString += english[i];
+                        break;
+                    }
             }
             trString += " ";
         }
@@ -29,6 +31,17 @@ public class MorseCodeDecoder {
     }
 
     public static void main(String[] args) {
-        System.out.println(decode("...---... -.-.--   - .... .   --.- ..- .. -.-. -.-   -... .-. --- .-- -.   ..-. --- -..-   .--- ..- -- .--. ...   --- ...- . .-.   - .... .   .-.. .- --.. -.--   -.. --- --. .-.-.-"));
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out
+                .println("----------------------------------MORSE CODE DECODER---------------------------------------");
+        System.out.println("ENTER THE MORSE CODE: ");
+        System.out.println(
+                "NOTE: THE LETTERS MUST BE SEPERATED BY A SPACE AND THE WORDS MUST BE SEPERATED IN THIS MANNER ' / '.");
+        String morse = sc.nextLine();
+        System.out.println("DECODED MESSAGE: " + decode(morse));
+
+        sc.close();
     }
 }
